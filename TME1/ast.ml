467eafdb66b4ecbcc15b4c84f85c3927b31293d5
LI SUXUE <3520716@ppti-14-407-14.ufr-info-p6.jussieu.fr>
2019-02-08 19:19:43 +0100

    Reprise TME1

--- TME1/ast.ml
+++ TME1/ast.ml
@@ -1,5 +1,11 @@
 type expr = 
 	ASTNum of int 
-	|ASTPrint of expr
-type prog = expr
+	
+type instr = 
+	ASTPrint of expr
+	 
+type prog = ASTProg of cmd list 
 
+type cmd = ASTExpr of expr
+	|ASTInstr of instr * cmd list
+	|ASTExpr of expr * cmd list 
---
+++ TME2/machine.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 16:15:24 2019
+
+@author: SUXUE LI/JULIEN XAVIER
+"""
+
