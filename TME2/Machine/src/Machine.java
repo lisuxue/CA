53703a2af75d20eab7ccaf43595119a5e2bd93e2
3521024 <3521024@ppti-14-508-01.ufr-info-p6.jussieu.fr>
2019-02-20 20:08:39 +0100

    a finir : manque le 13eme op + implem du main + details

--- TME2/Machine/src/Machine.java
+++ TME2/Machine/src/Machine.java
@@ -2,6 +2,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Machine {
@@ -87,25 +89,41 @@ public class Machine {
 
 
 	public void ortho(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 
 
 	public void load_prog(int a,int b,int c) {
-		// TODO Auto-generated method stub
-		
+
+		ArrayList<Integer> copie = (ArrayList<Integer>)(coll_tab_plateaux.get(registres[b]).clone());
+		coll_tab_plateaux.set(0, copie);
+		indExec = registres[c];
+	
 	}
 
 
 
 	public void enter(int a,int b,int c) {
 		Scanner sc = new Scanner(System.in);
-		if(sc.hasNext()) {
-			registres[c] = sc.nextInt();
-		}else {
-			registres[c] = Integer.parseInt(Integer.toBinaryString(Integer.MAX_VALUE));
+		Integer entree = null;
+		Boolean fin = false;
+		if(sc.hasNextLine()) {
+			String s = sc.nextLine();
+			if(s.length() == 0) {
+				registres[c] = 0xFFFFFFFF;
+			}else {
+				try {
+					entree = Integer.parseInt(s);
+				}catch(Exception e) {
+					fin = true; //on considère que tout les entrées qui ne correspondent pas à un format integer, sont prit comme fin 
+				}
+			if(fin)
+				registres[c] = 0xFFFFFFFF;
+			else
+				registres[c] = entree;
+			}
 		}
 		sc.close();
 	}
@@ -114,23 +132,23 @@ public class Machine {
 
 	public void exit(int a,int b,int c) {
 		if(registres[c]<=255) {
-			if(registres[C]>= 0 ) {
+			if(registres[c]>= 0 ) {
 				System.out.println(registres[c]);
 			}
-		}
+		}else
+			System.out.println("Error exit");
 	}
 
 
 
 	public void aband(int a,int b,int c) {
-		// TODO Auto-generated method stub
 		coll_tab_plateaux.remove((int)registres[c]);
 	}
 
 
 
 	public void alloc(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		ArrayList<Integer> l = new ArrayList<>(registres[c]);
 		for (int i = 0 ;i<l.size();i++) {
 			l.set(i,0);
@@ -142,65 +160,65 @@ public class Machine {
 
 
 	public void stop(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		System.exit(0);
 	}
 
 
 
 	public void not_and(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		registres[a] = ~(registres[b] & registres[c]); 
 	}
 
 
 
 	public void div(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		registres[a]= (int) ((registres[b] / registres[c]) % Math.pow(2,32));
 	}
 
 
 
 	public void mul(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		registres[a]= (int) ((registres[b] * registres[c]) % Math.pow(2,32));
 	}
 
 
 
 	public void add(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		registres[a]= (int) ((registres[b] + registres[c]) % Math.pow(2,32));
 	}
 
 
 
 	public void mod_tab(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		coll_tab_plateaux.get(registres[a]).set(registres[b],registres[c]);
 	}
 
 
 
 	public void ind_tab(int a,int b,int c) {
-		// TODO Auto-generated method stub
+
 		registres[a] = coll_tab_plateaux.get(registres[b]).get(registres[c]);
 	}
 	
 	private void mouv_cond(int a, int b, int c) {
-		// TODO Auto-generated method stub
+
 		if(registres[c]!=0)
 			registres[a]=registres[b];
 	}
 
-	public ArrayList<String> read(String filename) {
-		ArrayList<String> l = new ArrayList<>();
+	public ArrayList<byte[]> read(String filename) {
+		ArrayList<byte[]> l = new ArrayList<>();
 		try {
 			FileInputStream f = new FileInputStream(filename);
 			byte[] bTab = new byte[4];
 			while (f.read(bTab) != -1) {
-				System.out.println(bTab[0]);
+				l.add(bTab);
 			}
 			f.close();
 		} catch (FileNotFoundException e) {
@@ -217,12 +235,43 @@ public class Machine {
 		/*La machine lit le contenu du parchemin de programme et le stocke dans coll_tab_plateaux[0][0]
 		 * 
 		 * */
-		System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+		//System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
+		byte[] b = new byte[4];
 		System.out.println("avant le read");
-		ArrayList<String> l = m1.read("src/benchsum/fact.um");
-		for(int i=0;i<l.size();i++) {
+		ArrayList<byte[]> l = m1.read("src/benchsum/fact.um");
+		for(int i=0;i<l.size();i++) {   ///que faire ????
 			System.out.println("yo");
 			System.out.println(l.get(i));
 		}
+		
+		
+		
 	}
 }
+
+
+
+/*
+ * System.out.println("kjhkkjkjk");
+			try {
+				entree = sc.nextInt();
+				System.out.println(entree);
+			}catch(InputMismatchException e) {  //le type de donnée en entrée n'est pas integer 
+				if((sc.nextLine()).length() == 0 )  // si la ligne est vide, fin d'entree signalée
+					fin = true;
+				else 
+					fin = true;
+			}catch(NoSuchElementException e) {
+				System.out.println("Le flux est terminé");
+			}catch(IllegalStateException e) {
+				System.out.println("Le scanner est déjà fermé");
+			}finally {
+				if(fin) { //fin signalée
+					//registres[c] = 0xFFFFFFFF;
+					System.out.println("false pas dentree");
+				}else {
+					//registres[c] = entree;
+					System.out.println("jgjhgjgj");
+				}
+			}
+ * */
