ffac66d9671108c183c799e611ec31e4ea67ecf7
LI SUXUE <3520716@ppti-14-406-01.ufr-info-p6.jussieu.fr>
2019-02-28 18:12:55 +0100

    refait record

--- Projet/parser.ml
+++ Projet/parser.ml
@@ -1,5 +1,8 @@
-
-type triplet = (string * (string * string list))
+type triplet = {
+	label:string;
+	instr:string;
+	args:string list
+}
 
 (*
 permet de matcher directement sur les noms des champs par exemple :
@@ -22,24 +25,25 @@ let rec print_list_string =
 	| e::l -> e^"::"^print_list_string l
 
 (* val print_triplet : triplet -> unit*)
-let print_triplet (label,(instr,args)) =
-	Printf.printf "(label:\"%s\" , (instr:\"%s\" , args : %s))" label instr (print_list_string args);print_newline ()
+let print_triplet {label;instr;args} =
+	Printf.printf "{label:\"%s\" , instr:\"%s\" , args : %s}" label instr (print_list_string args);print_newline ()
 
 
-(* val make_tuple : string list -> triplet affiche le triplet correspondant à la ligne donnée*)
-let make_tuple l =
+(* val make_record : string list -> triplet affiche le triplet correspondant à la ligne donnée*)
+let make_record l =
 		let fst = List.hd l in
 		let label_length = (String.length fst) - 1 in
 			if String.contains fst ':' then
-				(String.sub (List.hd l) 0 label_length,(List.hd (List.tl l),List.tl (List.tl l)))
+				{label= String.sub (List.hd l) 0 label_length; instr=List.hd (List.tl l) ; args=List.tl (List.tl l)}
 			else
-  	    		("",(List.hd l,List.tl l))
+  	    		{label= ""; instr=List.hd l; args=List.tl l}
 
 (* val scan : in_channel -> triplet : enlève les séparateurs de la ligne  *)
 let scan line =
 	let delim = Str.regexp "[, \t]+" in
 		 let liste = Str.split delim line in
-		 	  make_tuple liste
+		 	  make_record liste
+
 
 (* val parse : in_channel -> triplet list *)
 let parse fichier =
@@ -57,7 +61,5 @@ let rec print_prog list_triplet =
 	match list_triplet with 
 		| [] -> ()
 		| h::t -> print_triplet h;
-				  print_prog t;; 
-				  
-print_prog (parse Sys.argv.(1));;
+				  print_prog t
 
