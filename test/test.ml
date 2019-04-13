31373256f1390b373006f6e6ad0f371b543ad14e
Suxue LI <lisuxueli@gmail.com>
2019-04-13 22:12:02 +0200

    maj dossier

--- test/test.ml
+++
@@ -1,6 +0,0 @@
-let entier = ref 0
-
-let plus n =
-  entier:=(!entier)+n
-
-let print_entier n = plus n;print_int !entier;print_newline ();
--- test/test2.ml
+++
@@ -1,3 +0,0 @@
-open Test;;
-
-let _ = Test.print_entier 3;print_int Test.(!entier);
